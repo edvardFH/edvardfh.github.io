e1bb690b059d5cd563e28d13856fcb74b702d9b0
edvardFH <97302653+edvardFH@users.noreply.github.com>
2025-07-06 16:27:19 +0200

    Test -> wip first chart

---
+++ assets/WindowWidth-CXUg7Uy1.js
@@ -0,0 +1,1 @@
+import{r as d,a as o,o as r,p as s}from"./index-v8lp9L4R.js";const a=600;function w(){const e=d(window.innerWidth);function n(){e.value=window.innerWidth}const i=o(()=>e.value>a),t=o(()=>!i.value);return r(()=>window.addEventListener("resize",n)),s(()=>window.removeEventListener("resize",n)),{windowWidth:e,isScreenLarge:i,isScreenSmall:t}}export{w as u};
---
+++ assets/pinia-BZ6JmNzK.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-v8lp9L4R.js";import{c as t}from"./pinia-PIRJWMon.js";const r=a(({app:i})=>{const o=t();i.use(o)});export{r as default};
